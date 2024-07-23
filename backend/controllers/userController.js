2dd65cc8bebf2d8c3bfb2017da4b24e491fab024
Himanshu-coder007 <himanshunagose54@gmail.com>
2024-07-23 19:33:33 +0530

    login api is created successfully

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import { User } from "../models/userSchema.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const Register = async (req, res) => {
   try {
@@ -33,3 +34,40 @@ export const Register = async (req, res) => {
     console.log(error)
   }
 };
+
+export const Login = async (req,res) => {
+  try {
+    const {email,password} = req.body;
+    if(!email || !password){
+      return res.status(401).json({
+        message: "All fields are required.",
+        success:false
+      })
+    };
+    const user = await User.findOne({email}); 
+    if(!user){
+      return res.status(401).json({
+        message: "Incorrect email or password",
+        success: false
+      })
+    }
+    const isMatch = await bcryptjs.compare(password,user.password);
+    if(!isMatch){
+      return res.status(401).json({
+        message: "Incorrect email or password",
+        success:false
+      });
+    }
+    const tokenData = {
+      userId:user._id
+    }
+    const token = await jwt.sign(tokenData,process.env.TOKEN_SECRET,{expiresIn:"1d"})
+    return res.status(210).cookie("token",token, {expiresIn:"1d", httpOnly:true}).json({
+        message: `Welcome back ${user.name}`,
+        success: true
+    })
+  }
+  catch(error){
+    console.log(error);
+  }
+}
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
-import {Register} from "../controllers/userController.js";
+import {Register,Login} from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.route("/register").post(Register);
+router.route("/login").post(Login);
 
 export default router;
