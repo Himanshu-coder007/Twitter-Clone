3249c666b35401b06dcbd1c3d0bd012d1bbd8c3e
Himanshu-coder007 <himanshunagose54@gmail.com>
2024-07-25 16:01:34 +0530

    likeordislike api is added successfully

--- backend/routes/tweetRoute.js
+++ backend/routes/tweetRoute.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { createTweet,deleteTweet } from "../controllers/tweetController.js";
+import { createTweet,deleteTweet, likeOrDislike } from "../controllers/tweetController.js";
 import isAuthenticated from "../config/auth.js";
 
 const router = express.Router();
 
 router.route("/create").post(isAuthenticated,createTweet)
 router.route("/delete/:id").delete(isAuthenticated, deleteTweet);
+// router.route("like/:id").put(isAuthenticated,likeOrDislike);
+router.route("/like/:id").put(isAuthenticated, likeOrDislike);
 export default router;
