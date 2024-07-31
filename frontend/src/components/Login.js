d768c832489f985421783e855c19ad3e4cc2bfb7
Himanshu-coder007 <himanshunagose54@gmail.com>
2024-07-31 23:50:38 +0530

    some changes in front end Login and register

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,7 +1,28 @@
 import React, { useState } from "react";
+import axios from "axios"
+import { USER_API_END_POINT } from "../utils/constant";
 
 const Login = () => {
   const [isLogin,setIsLogin] =useState(true);
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if(isLogin){
+        // loginn
+    }else {
+        // signup
+        try {
+
+        } catch (error) {
+          console.log(error);
+        }
+    }
+  }
 
 
   const loginSignupHandler = () => {
@@ -25,29 +46,39 @@ const Login = () => {
             <h1 className="font-bold text-6xl">Happening now</h1>
           </div>
 
-          <h1 className="mt-4 mb-2 text-2xl font-bold">{isLogin ? "Login" : "Signup"}</h1>
-          <form className="flex flex-col w-[55%]">
+          <h1 className="mt-4 mb-2 text-2xl font-bold">
+            {isLogin ? "Login" : "Signup"}
+          </h1>
+          <form onSubmit={submitHandler} className="flex flex-col w-[55%]">
             {!isLogin && (
               <>
                 <input
                   type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
                   placeholder="Name"
                   className="outline-blue-500 border border-gray-800 px-3 py-2 rounded-full my-1 font-semibold"
                 />
                 <input
                   type="text"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
                   placeholder="Username"
                   className="outline-blue-500 border border-gray-800 px-3 py-2 rounded-full my-1 font-semibold"
                 />
               </>
             )}
             <input
-              type="text"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               placeholder="Email"
               className="outline-blue-500 border border-gray-800 px-3 py-2 rounded-full my-1 font-semibold"
             />
             <input
-              type="text"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               placeholder="Password"
               className="outline-blue-500 border border-gray-800 px-3 py-2 rounded-full my-1 font-semibold"
             />
@@ -56,7 +87,12 @@ const Login = () => {
             </button>
             <h1>
               {isLogin ? "Do not have an account?" : "Already have an account?"}
-              <span onClick={loginSignupHandler} className="font-bold text-blue-600 cursor-pointer ml-1">{isLogin ? "Signup" : "Login"}</span>
+              <span
+                onClick={loginSignupHandler}
+                className="font-bold text-blue-600 cursor-pointer ml-1"
+              >
+                {isLogin ? "Signup" : "Login"}
+              </span>
             </h1>
           </form>
         </div>
