576193837704a09f41e4cdffa6d1856a0528fb64
Himanshu-coder007 <himanshunagose54@gmail.com>
2024-07-31 12:17:53 +0530

    middleware and route for getalltweet is successfully written

--- backend/controllers/tweetController.js
+++ backend/controllers/tweetController.js
@@ -65,3 +65,59 @@ export const likeOrDislike = async (req,res) => {
     }
 };
 
+// export const getAllTweets = async (req, res) => {
+//     //loggedInUser's tweet + following user tweet
+//     try {
+//         const id = req.params.id;
+//         const loggedInUser = await User.findById(id);
+//         const loggedInUserTweets = await Tweet.findByIdAndUpdate({userId:id});
+//         const followingUserTweet = await Promise.all(loggedInUser.following.map((otherUsersId)=>{
+//             return Tweet.findByIdAndUpdate({userId:otherUsersId});
+//         }));
+//         return res.status(200).json({
+//             tweets:loggedInUserTweets.concat(...followingUserTweet),
+//         })
+//     } catch(error){
+//         console.log(error);
+//     }
+// }
+
+export const getAllTweets = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const loggedInUser = await User.findById(id);
+    if (!loggedInUser) {
+      return res.status(404).json({
+        message: "Logged in user not found",
+        success: false,
+      });
+    }
+
+    // Find all tweets by the logged-in user
+    const loggedInUserTweets = await Tweet.find({ userId: id });
+
+    // Find all tweets by the users that the logged-in user is following
+    const followingUserTweets = await Promise.all(
+      loggedInUser.following.map(async (otherUserId) => {
+        return Tweet.find({ userId: otherUserId });
+      })
+    );
+
+    // Flatten the array of tweets from following users
+    const allFollowingTweets = followingUserTweets.flat();
+
+    // Combine the logged-in user's tweets with those from following users
+    const allTweets = loggedInUserTweets.concat(allFollowingTweets);
+
+    return res.status(200).json({
+      tweets: allTweets,
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "An error occurred",
+      success: false,
+    });
+  }
+};
--- backend/routes/tweetRoute.js
+++ backend/routes/tweetRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createTweet,deleteTweet, likeOrDislike } from "../controllers/tweetController.js";
+import { createTweet,deleteTweet, getAllTweets, likeOrDislike } from "../controllers/tweetController.js";
 import isAuthenticated from "../config/auth.js";
 
 const router = express.Router();
@@ -7,4 +7,5 @@ const router = express.Router();
 router.route("/create").post(isAuthenticated,createTweet)
 router.route("/delete/:id").delete(isAuthenticated, deleteTweet);
 router.route("/like/:id").put(isAuthenticated, likeOrDislike);
+router.route("/alltweets/:id").get(isAuthenticated,getAllTweets);
 export default router;
