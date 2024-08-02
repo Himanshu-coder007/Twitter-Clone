341c9ab250697e0c33339216502ce30c7d40b3cf
Himanshu-coder007 <himanshunagose54@gmail.com>
2024-08-02 19:03:16 +0530

    who to follow is added

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,17 +3,21 @@ import LeftSidebar from './LeftSidebar'
 import RightSidebar from './RightSidebar'
 
 import { Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import useOtherUsers from '../hooks/useOtherUser';
 
 
 
 const Home = () => {
-  
+  //custom hook
+  const {user,otherUsers} = useSelector(store=>store.user);
+  useOtherUsers(user?._id);
   
   return (
     <div className='flex justify-between w-[80%] mx-auto'>
       <LeftSidebar />
       <Outlet/>
-      <RightSidebar />
+      <RightSidebar otherUsers={otherUsers}/>
     </div>
   );
 }
